25ab126314ca8f855cc536093e00ebdf9dd8a378
Nanjun701 <liun266@newschool.edu>
2025-11-05 14:26:31 -0500

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/25lab-project-3-reactjs/'
+  base:'/25-lab-reactjs/'
 })
